d022f6b7b7e68fe817ffa28f2602c8296f509d4a
jack <j.mcmahon@outlook.ie>
2026-01-28 23:46:09 +0000

    rename files

---
+++ build_out_q_learning_table.py
@@ -0,0 +1,66 @@
+from xo_functions import get_possible_moves, check_for_winner
+import re
+import pandas as pd
+
+remove = "[\[\]\, \(\)]"
+def create_name(state):
+    return re.sub(remove, "", str(state)) #TODO: is this more performant than just doing a lot of .replace statements
+
+init = [[0,0,0],[0,0,0],[0,0,0]]
+possible_states = [[init]]
+previous_states = [init]
+possible_action_count = 0
+
+
+cols = ["state", "00", "01", "02", "10", "11", "12", "20", "21", "22"]
+df = pd.DataFrame(columns = cols)
+new_rows = []
+
+i = 0
+# how many possible "start of turn" states are there
+for turn in range(0,9):
+    turn += 1
+    if turn % 2 == 0:
+        p = 1
+    else:
+        p = -1
+    print(f"Turn {turn}. Player {p}")
+    new_states = []
+    for starting_state in previous_states:
+        print(f"State: {i}", end='\r')
+        i += 1
+        new_row = pd.DataFrame(columns = cols)
+        new_row["state"] = [str(starting_state)]
+        actions = get_possible_moves(starting_state)
+        for action in actions:
+            possible_action_count += 1
+            action_str = create_name(action)
+            new_row[action_str] = [0]
+            new_state = [row[:] for row in starting_state] # Thank you, random redditor, for your help here
+            #https://www.reddit.com/r/learnpython/comments/1imbmpo/changing_one_variable_automatically_changes/
+            new_state[action[0]][action[1]] = p
+            if check_for_winner(new_state) is False: # If it is a winning move, the game is over, we don't need to take this branch further
+                new_states.append(new_state)
+        new_rows.append(new_row)
+    possible_states.append(new_states)
+    previous_states = new_states
+
+print(f"Length of new_rows: {len(new_rows)}") # 294,778
+df = pd.concat(new_rows, ignore_index= True)
+print(df)
+df = df.drop_duplicates() #TODO: there might be a way to write the code above so that no duplicates are created in the first place
+print(df)
+df.to_csv("q_learning_table.csv", index = False)
+
+### Everything below this point was part of an approach which in the end was not necessary
+possible_states_flat = []
+for possible_state_list in possible_states:
+    for possible_state in possible_state_list:
+        possible_states_flat.append(possible_state)
+
+print(f"Length of possible_states_flat: {len(possible_states_flat)}") # This was 986,410 before pruning out winning games
+#Now it is 340,858
+
+print(f"Number of possible actions to train for: {possible_action_count}") # 549,945
+# The length of df and the number of possible states does not match.
+# This is because the list of states include winning and end states which do not appear in df.
---
+++ play_against_computer.py
@@ -0,0 +1,104 @@
+from xo_functions import make_player_move, make_computer_move, prettify_board, Player
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--opponent", help = "what model to play against")
+args = parser.parse_args()
+opponent = args.opponent
+
+if opponent == "rl":
+    p2 = Player("Gregg","q_learning_table.csv","NOT_LEARNING") # Static trained model
+else:
+    p2 = Player("Gregg (P2)","blank_q_learning_table.csv","NOT_LEARNING")
+
+
+b = [[0,0,0],[0,0,0],[0,0,0]]
+
+x_or_o = input("Are you X (going first) or O (going second)? ").upper()
+if x_or_o == "X":
+    print("You are X")
+    p = -1
+    p_name = "X"
+    c = 1
+    c_name = "O"
+elif x_or_o in ["O", "0"]:
+    print("You are O")
+    p = 1
+    p_name = "O"
+    c = -1
+    c_name = "X"
+else:
+    print("Invalid input")
+
+print("""
+    Here are the inputs:
+    
+     --------------
+    | a1 | a2 | a3 |
+     --------------
+    | b1 | b2 | b3 |
+     --------------
+    | c1 | c2 | c3 |
+     --------------
+      
+    To quit, enter "q"
+    """)
+
+
+if p == -1:
+    turns_taken = 0
+    print("You are going first")
+    m = input("Input your move: ")
+    if m == "q":
+        exit()
+
+    b, status = make_player_move(p,b,m,turns_taken)
+    turns_taken += 1
+    print("The Computer is making its first move")
+    b, status, description, m = p2.make_agent_move(b,turns_taken,1)
+    #b, status = make_computer_move(c,b,turns_taken)
+    turns_taken += 1
+
+if p == 1:
+    turns_taken = 0
+    print("The Computer is going first")
+    #b, status = make_computer_move(-1,b,turns_taken)
+    b, status, description, m = p2.make_agent_move(b,turns_taken,-1)
+    turns_taken = 1
+
+status = False
+winner = 0
+
+while turns_taken != 9 and status == 0:
+    print(f"Your move. Here is the board. You are {p_name} and The Computer is {c_name}")
+
+    prettify_board(b)
+
+    valid_move = False
+    while valid_move is False:
+        try:
+            m = input("Input your move: ")
+            if m == "q":
+                exit()
+            b, status = make_player_move(p,b,m,turns_taken)
+            turns_taken += 1
+            valid_move = True
+            print("That's a valid move")
+            if status == True:
+                print("\nCongrats! You Won!")
+                winner = p
+        except ValueError:
+            print("An error occurred, try again")
+    if turns_taken != 9 and status == 0:
+        b, status, description, m = p2.make_agent_move(b,turns_taken,c)
+        #b, status = make_computer_move(c,b,turns_taken)
+        turns_taken += 1
+        if status == True:
+            print("\nOh no! The Computer won :(")
+            winner = c
+
+if status == 0:
+    print("It was a stalemate")
+print("\nHere's the final board:")
+prettify_board(b)
+
