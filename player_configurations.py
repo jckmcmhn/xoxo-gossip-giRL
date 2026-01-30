1b8e0d62a6974cd1fa4bc6e7d491cb003e6131bc
jack <j.mcmahon@outlook.ie>
2026-01-30 21:51:49 +0000

    fix

--- player_configurations.py
+++ player_configurations.py
@@ -1,5 +1,5 @@
-p1 = Player("Tom (P1)",file,"LEARNING_DEMO", reward_win)
-p1 = Player("Tom (P1)",file,"LEARNING", reward_win)
+p1 = Player("Tom (P1)",file,"LEARNING_DEMO", rewards)
+p1 = Player("Tom (P1)",file,"LEARNING", rewards)
 p1 = Player("Tom (P1)",file,"FIXED") # Static trained model
 p2 = Player("Gregg (P2)","blank_q_learning_table.csv","FIXED")
 p2 = Player("Al (P2)","blank_q_learning_table.csv","RULES_IMPERFECT")
