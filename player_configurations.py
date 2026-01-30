0189ebfc348b70d5741038843fd332db270ef0c9
jack <j.mcmahon@outlook.ie>
2026-01-30 12:59:32 +0000

    don't need file for rules_imperfect

--- player_configurations.py
+++ player_configurations.py
@@ -2,4 +2,4 @@ p1 = Player("Tom (P1)",file,"LEARNING_DEMO", reward_win)
 p1 = Player("Tom (P1)",file,"LEARNING", reward_win)
 p1 = Player("Tom (P1)",file,"NOT_LEARNING") # Static trained model
 p2 = Player("Gregg (P2)","blank_q_learning_table.csv","NOT_LEARNING")
-p2 = Player("Al (P2)",file,"RULES_IMPERFECT")
+p2 = Player("Al (P2)","blank_q_learning_table.csv","RULES_IMPERFECT")
