dfee09a225661811a08d32e1e34a63f676e7b8b0
jack <j.mcmahon@outlook.ie>
2026-01-31 16:24:31 +0000

    more qol fixes

--- run_training_job.py
+++ run_training_job.py
@@ -11,6 +11,7 @@ parser.add_argument("-o", "--out_file", help = "What file are you using to save 
 parser.add_argument("-n", "--number_of_episodes", help = "How many training episodes to run?")
 parser.add_argument("-r", "--rewards", help = "What rewards are applied. Format: w|l|d Sample: '[20|-10|10]' ")
 parser.add_argument("-e", "--epsilon", help = "todo")
+parser.add_argument("-t", "--training", help = "Are you training a model in this run: 1 for True 0 for False")
 
 # Read arguments from command line
 args = parser.parse_args()
@@ -24,9 +25,13 @@ rewards = args.rewards.replace("[","").replace("]","").split("|")
 reward_win, reward_lose, reward_draw = rewards
 reward_win, reward_lose, reward_draw = float(reward_win), float(reward_lose), float(reward_draw)
 rewards = [reward_win, reward_lose, reward_draw]
+n = int(args.number_of_episodes)
+print(args.training)
+training = args.training == 1
 
-training = True #TODO: Make argument
+
 if training:
+    print("Training")
     # For now, always make P1 the model player you are most interested in assesing, with p2 as the benchmark
     p1 = Player("Tom Cruise's character from Live Die Repeat / Edge Of Tomorrow (P1)","LEARNING", in_file, out_file, epsilon, rewards)
     p1.greet()
@@ -39,6 +44,7 @@ print("\n------------\n")
 print("VALIDATION STEP")
 print("\n------------\n")
 n_validation = int(n / 10) # Hope this doesn't "nvalidate" the training approach!
+n_validation = 2000 # Hope this doesn't "nvalidate" the training approach!
 
 #print("Introducing... the challenger!")
 test = 0
@@ -55,7 +61,7 @@ p1 = Player("Tom (P1)","FIXED", out_file)
 p1.greet()
 p2 = Player("Gregg (P2)","FIXED")
 p2.greet()
-p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, int(n / 10), "ALTERNATE", 0)
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
 p1_validation_wins += p1_wins
 p1_validation_loses += p2_wins
 validation_draws += draws
@@ -69,7 +75,7 @@ p1 = Player("Tom (P1)","FIXED", out_file)
 p1.greet()
 p2 = Player("Al (P2)","RULES_IMPERFECT")
 p2.greet()
-p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, int(n / 10), "ALTERNATE", 0)
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
 p1_validation_wins += p1_wins
 p1_validation_loses += p2_wins
 validation_draws += draws
@@ -77,13 +83,13 @@ validation_headlines.append(headline)
 
 test += 1
 print("\n----------")
-print(f"TEST {test}: Trained model vs imperfect rules")
+print(f"TEST {test}: Trained model vs distracted imperfect rules")
 print("----------")
 p1 = Player("Tom (P1)","FIXED", out_file)
 p1.greet()
 p2 = Player("Al (P2)","RULES_IMPERFECT_NOT_LOCKED_IN")
 p2.greet()
-p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, int(n / 10), "ALTERNATE", 0)
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
 p1_validation_wins += p1_wins
 p1_validation_loses += p2_wins
 validation_draws += draws
@@ -97,14 +103,43 @@ p1 = Player("Tom (P1)","FIXED", out_file)
 p1.greet()
 p2 = Player("Hennimore (P2)","FIXED", "weakly_trained.csv")
 p2.greet()
-p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, int(n / 10), "ALTERNATE", 0)
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
 p1_validation_wins += p1_wins
 p1_validation_loses += p2_wins
 validation_draws += draws
 validation_headlines.append(headline)
 # python .\run_training_job.py -i blank_q_learning_table.csv -o weakly_trained.csv --reward '[10|0|5]' -e 70 -n 100
 
-print(f"\nFor this experiment, there were {n} training episodes and {n_validation} validation episodes, epsilon was {epsilon}, the win reward was {reward_win}, the loss reward was {reward_lose} and the draw reward was {reward_draw}\n")
+test += 1
+print("\n----------")
+print(f"Extra Test: Trained model vs itself")
+print("----------")
+p1 = Player("Tom (P1)","FIXED", out_file)
+p1.greet()
+p2 = Player("Tom (P2)","FIXED", out_file)
+p2.greet()
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
+p1_validation_wins += p1_wins
+p1_validation_loses += p2_wins
+validation_draws += draws
+validation_headlines.append(headline)
+
+test += 1
+print("\n----------")
+print(f"Extra Test: Trained model vs the champ")
+print("----------")
+p1 = Player("Tom (P1)","FIXED", out_file)
+p1.greet()
+p2 = Player("The Champ (P2)","FIXED", "experiments/20260131_1_well_trained.csv")
+p2.greet()
+p1_wins, p2_wins, draws, headline = run_training_loop(p1, p2, n_validation, "ALTERNATE", 0)
+p1_validation_wins += p1_wins
+p1_validation_loses += p2_wins
+validation_draws += draws
+validation_headlines.append(headline)
+
+print("#THE HEADLINES")
+print(f"\nFor this experiment, there were {n} training episodes and {n_validation} validation episodes per benchmark, epsilon was {epsilon}, the win reward was {reward_win}, the loss reward was {reward_lose} and the draw reward was {reward_draw}\n")
 print(f"Across {test * n_validation} validation episodes, p1 won {p1_validation_wins} times ({100 * round(p1_validation_wins / (test * n_validation), 2)}%)")
 print(f"Across {test * n_validation} validation episodes, p1 lost {p1_validation_loses} times ({100 * round(p1_validation_loses / (test * n_validation), 2)}%)")
 print(f"Across {test * n_validation} validation episodes, there were {validation_draws} draws ({100 * round(validation_draws / (test * n_validation), 2)}%)")
