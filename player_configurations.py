9c2e4895ca2d3a3a6bc0ae4852955089c550c11c
jack <j.mcmahon@outlook.ie>
2026-02-01 15:49:26 +0000

    missing table

--- player_configurations.py
+++
@@ -1,5 +0,0 @@
-p1 = Player("Tom (P1)",file,"LEARNING_DEMO", rewards)
-p1 = Player("Tom (P1)",file,"LEARNING", rewards)
-p1 = Player("Tom (P1)",file,"FIXED") # Static trained model
-p2 = Player("Gregg (P2)","blank_q_learning_table.csv","FIXED")
-p2 = Player("Al (P2)","blank_q_learning_table.csv","RULES_IMPERFECT")
